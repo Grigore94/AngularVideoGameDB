cc8732f2de7c5556994ab41ca4398a3ddec690ed
Grigore <grigore.mereacre95@gmail.com>
2021-06-25 10:05:28 -0500

    added styles to search bar component

--- ng-video-game-db/src/app/app.module.ts
+++ ng-video-game-db/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
