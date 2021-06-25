6456a87469174ce64fb8d346883c1304b2e713bb
Grigore <grigore.mereacre95@gmail.com>
2021-06-25 11:46:45 -0500

    seted env variables created a modules file

--- ng-video-game-db/src/app/services/http.service.ts
+++ ng-video-game-db/src/app/services/http.service.ts
@@ -1,5 +1,7 @@
-import { HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment as env } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +9,15 @@ import { Injectable } from '@angular/core';
 export class HttpService {
 
   constructor(private http: HttpClient) { }
-  hetGAmeList(
+  getGameList(
     ordering: string,
     search?: string
-  );Observable<APIResponse<Game>>{
+  ): Observable<APIResponse<Game>> {
     let params = new HttpParams().set("ordering", ordering);
     if(search) {
       params = new HttpParams().set("ordering", ordering).set("search", search)
     }
-    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`,{
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params: params,
     })
   }
