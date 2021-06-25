878818b2726cee3c7c0a89b8cb179069b93674ed
Grigore <grigore.mereacre95@gmail.com>
2021-06-25 11:58:07 -0500

    created all the models need for the aplication an created all the interfaces

---
+++ ng-video-game-db/src/app/services/modules.ts
@@ -0,0 +1,44 @@
+export interface Game {
+    backgroung_image: string;
+    name: string;
+    released:string;
+    metacritic_url: string;
+    website: string;
+    description: string;
+    metacritic: number;
+    genres: Array<Genre>;
+    parent_platform: Array<ParentPlatform>;
+    publisher: Array<Publishers>;
+    ratings: Array<Raiting>;
+    screenshots: Array<Screenshoots>;
+    trailers: Array<Trailer>''
+
+}
+
+export interface APIResponse<T> {
+    results: Array<T>;
+}
+interface Genre {
+    name: string;
+}
+interface ParentPlatform {
+    platform: {
+        name: string;
+    }
+}
+interface Publishers {
+name: string;
+}
+interface Rating {
+    id: number;
+    count: number;
+    title: string;
+}
+interface Screenshots {
+    image: string;
+}
+interface Trailer {
+    data: {
+        max: string;
+    }
+}
