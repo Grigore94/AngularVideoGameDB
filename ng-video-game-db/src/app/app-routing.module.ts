f748ebb19719746ec697b13a3b8014a8cffae25b
Grigore <grigore.mereacre95@gmail.com>
2021-06-25 16:24:35 -0500

    generated new component with CLI and in routing added the padh and comonent

--- ng-video-game-db/src/app/app-routing.module.ts
+++ ng-video-game-db/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
 
 
@@ -11,11 +12,16 @@ const routes: Routes = [
   {
     path: "search/game-search",
     component: HomeComponent,
+  },
+  {
+    path: "services/:id",
+    component: DetailsComponent,
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+eximport { DetailsComponent } from './components/details/details.component';
+ports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ng-video-game-db/src/app/app.module.ts
+++ ng-video-game-db/src/app/app.module.ts
@@ -15,12 +15,14 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component';
 import { HttpErrorsInterceptor } from './interceptors/http-erros.interceptor';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
 BrowserModule,
