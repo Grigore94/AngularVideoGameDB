fc7e290abf1fcb8012a261d531d03ab589e034e6
Grigore <grigore.mereacre95@gmail.com>
2021-06-25 13:26:17 -0500

    created a property of games and injected two private properties in constructor ,, ngOnInit onloading searching if there are any searchqueries calling the two params else only one ,, xreated searchGames method with two arg calling getgameLis method and subscribe

--- ng-video-game-db/src/app/components/home/home.component.ts
+++ ng-video-game-db/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-// public sort: string;
-  constructor() { }
+  public sort: string;
+  public games: Array<Game>;
+  constructor(
+    private httpService: HttpService,
+    private activateRoute: ActivateRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe((params: Params) => {
+      if (params["game-search"]) {
+        this.searchGames("metacrit", params["game-search"]);
+      }else {
+        this.searchGames("metacrit");
+      }
+    })
+  }
+  searchGames(sort: string, search?:string): void {
+    this.httpService
+    .getGameList(sort,search)
+    .subscribe((gameList: APIResponse<Game>) => {
+      this.games= gameList.results
+    })
   }
 
 }
